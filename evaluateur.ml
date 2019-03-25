668e58263eaa9019e0b4c187295d408b276b80f7
reihan35 <reihaneh77@yahoo.com>
2019-03-25 15:04:00 +0100

    debug

--- evaluateur.ml
+++ evaluateur.ml
@@ -1,87 +1,45 @@
 open Ast
 
-type V = InN of n
-		| InF of  Ast.expr * int * (string,V) list
-		| InFR of Ast.expr * int * (string,V) list 
-		|ERROR
-let v n = 
+type v = InN of Ast.expr
+		| InF of  Ast.expr * int * (string*v) list
+		| InFR of Ast.expr * int * (string*v) list 
+		| ERROR
+
+let v_e n = 
 	match n with 
 		|Ast.Int n -> n 
-		| _ -> "love you !"
+		| _ -> -100000000000000 (*Error*)
 
-let extend_ctx f args =
-	match e with 
-		|InF(e,0,l) -> l 
-		|InF(e,i,(t,_)::q) ->  (t,hd(args))::call(InF(e,i-1,q), tl(args))
-		|InF(e,i,[]) -> print_string("Should not happen") 
+(*
+let rec extend_ctx f args =
+	match f with 
+		|InF(f,0,l) -> l 
+		|InF(f,i,(t,_)::q) ->  (t,List.hd(args))::extend_ctx(InF(e,i-1,q), List.tl(args))
+		|InF(f,i,[]) -> print_string("Should not happen") 
+*)
 
 let toN v = 
 	match v with
 		|InN n -> n
-		|_-> (print_string("must be integer");ERROR)
+		|_-> Ast.Int(-100000000000001) (*(print_string("must be integer");ERROR)*)
 
-let pi e = 
-	match e with 
-		|Ast.Boolean e match e with 
-						|"true" -> 1
-						| "false" -> 0
-
-		|Ast.BinOperation(s,e,e') let e1=eval_expr(e), e2=eval_expr(e') in
-									match s with 
-									| "add" ->  toN(e1) + toN(e2)
-									| "mul" -> toN(e1) * toN(e2)
-									| "sub" -> toN(e1) - toN(e2)
-									| "div" -> toN(e1) // toN(e2)
-
-		|Ast.BoolOperation(s,e,e') let e1=eval_expr(e) in
-									match s with 
-									| "and" -> match e1 with
-												|InN(0) -> 0
-												|InN(1) -> eval_expr(e')
-									| "or" -> match e1 with
-												|InN(1) -> 1
-												|InN(0) -> eval_expr(e')
-		
-		|Ast.ComOperation(s,e,e') let e1=eval_expr(e), e2=eval_expr(e') in
-									match s with 
-									|"eq" when toN(e1)==toN(e2 -> 1 
-									|"eq" -> 0
-									|"lt" when toN(e) < toN(e2) -> 1
-									|"lt" -> 0
-
-		|Ast.UnOperation(s,e) let e1=eval_expr(e) in
-								match (s, e1) with 
-								|("not", InN(0)) -> 1
-								|("not", InN(1)) -> 0
-
-let rho s env = 
+let rec rho s env = 
 	match env with 
-		|[] -> []
+		|[] -> InN(Ast.Int(-10000002))
 		|(s,v)::q -> v
 		|(x,v)::q -> rho s q
 
 
-let eval_exprs e ctx = 
-	match e with
-		t::q -> eval_expr(t)::eval_exprs(q)
-		t::[] -> eval_expr(t)::[]
-
-let make_closure l  = 
-	match l with
-		| [] -> []
-		| (v,t)::q -> (v,_)::make_closure(q)
-		
-
 let eval_expr e ctx =
 	match e with
-		|Ast.Var(s) -> rho(s, ctx)
-		|Ast.Int n -> InN(v (n))
-		|Ast.Boolean "true" -> InN(1)
-		|Ast.Boolean "false" -> InN(0)
-		|Ast.BinOperation op -> InN(pi(op))
-		|Ast.ComOperation op -> InN(pi(op))
-		|Ast.BoolOperation op -> InN(pi(op))
-		|Ast.UnOperation op -> InN(pi(op))
+		|Ast.Int n -> InN(Ast.Int(n))
+		|Ast.Boolean true -> InN(Ast.Int(1))
+		|Ast.Boolean false -> InN(Ast.Int(0))
+		|Ast.Var s -> v_e(rho(s, ctx))
+		|Ast.BinOperation (op,_,_) -> InN(pi(op))
+		|Ast.ComOperation (op,_,_) -> InN(pi(op))
+		|Ast.BoolOperation (op,_,_) -> InN(pi(op))
+		|Ast.UnOperation (op,_)-> InN(pi(op))
 		|Ast.If (cnd, thn, el) when eval_expr(cnd) == 1 -> eval_expr(thn)
 		|Ast.If (cnd, thn, el) -> eval_expr(el)
 		|Ast.Call(f,exprs)-> let args = eval_exprs(exprs, ctx), f = eval_expr(f, ctx) in
@@ -96,11 +54,58 @@ let eval_expr e ctx =
 		|Ast.AnoFun(l,expr) -> InF(expr,length(l),make_closure(l))
 
 
+
+let pi e = 
+	match e with 
+		|Ast.BinOperation(s,e,e') -> let e1=eval_expr(e), e2=eval_expr(e') in
+									match s with 
+									| "add" ->  toN(e1) + toN(e2)
+									| "mul" -> toN(e1) * toN(e2)
+									| "sub" -> toN(e1) - toN(e2)
+									| "div" -> toN(e1) // toN(e2)
+
+		|Ast.Boolean b -> match b with 
+							|true -> 1
+							|false -> 0
+
+		|Ast.BoolOperation(s,e,e') -> let e1=eval_expr(e) in
+									match s with 
+									| "and" -> match e1 with
+												|InN(0) -> 0
+												|InN(1) -> eval_expr(e')
+									| "or" -> match e1 with
+												|InN(1) -> 1
+												|InN(0) -> eval_expr(e')
+		
+		|Ast.ComOperation(s,e,e') -> let e1=eval_expr(e), e2=eval_expr(e') in
+									match s with 
+									|"eq" when toN(e1)==toN(e2) -> 1 
+									|"eq" -> 0
+									|"lt" when toN(e) < toN(e2) -> 1
+									|"lt" -> 0
+
+		|Ast.UnOperation(s,e) -> let e1=eval_expr(e) in
+								match (s, e1) with 
+								|("not", InN(0)) -> 1
+								|("not", InN(1)) -> 0
+
+
+let eval_exprs e ctx = 
+	match e with
+		|t::q -> eval_expr(t)::eval_exprs(q)
+		|t::[] -> eval_expr(t)::[]
+
+let make_closure l  = 
+	match l with
+		| [] -> []
+		| (v,t)::q -> (v,t)::make_closure(q)
+		
+
 let eval_dec dec ctx = 
 	match d with 
 	|FunDec (s, t, l, e) -> (s, InF(e, length(l), make_closure(l)::ctx))
 	|ConstDec (s, t, e) -> (s, eval_expr(e))
-	|FunRecDec (s, t, l, e) -> (s, InFR(InF(e, length(l), (make_closure(l)::(s, _))::ctx))
+	|FunRecDec (s, t, l, e) -> (s, InFR(InF(e, length(l), (make_closure(l)::(s, t))::ctx)))
 
 
 let print_v v = 
